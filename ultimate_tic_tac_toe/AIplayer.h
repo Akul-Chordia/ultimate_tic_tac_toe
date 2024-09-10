2caadc0fe37a65706fd6d32fd782f18ea9cc7d6e
acool8868 <acool8868@gmail.com>
2024-09-11 01:19:11 +0530

    -> fixed error where user makes invalid move while choosing a board putting the program into an infinite loop -> added error handling to user inputs -> made player a part of ultimate class -> seperated code to seperate files for readibility

---
+++ ultimate_tic_tac_toe/AIplayer.h
@@ -0,0 +1,12 @@
+//
+//  AIplayer.h
+//  ultimate_tic_tac_toe
+//
+//  Created by Akul Chordia on 9/10/24.
+//
+
+#ifndef AIplayer_h
+#define AIplayer_h
+
+
+#endif /* AIplayer_h */
---
+++ ultimate_tic_tac_toe/tictactoe_class.h
@@ -0,0 +1,12 @@
+//
+//  tictactoe_class.h
+//  ultimate_tic_tac_toe
+//
+//  Created by Akul Chordia on 9/10/24.
+//
+
+#ifndef tictactoe_class_h
+#define tictactoe_class_h
+
+
+#endif /* tictactoe_class_h */
