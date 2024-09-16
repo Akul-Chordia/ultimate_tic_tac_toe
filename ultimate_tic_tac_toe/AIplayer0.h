5fa66856a915ff395856c43f51ee4a4de6bdaa7c
acool8868 <acool8868@gmail.com>
2024-09-16 12:20:48 +0530

    -> implemented 1st level of AI

---
+++ ultimate_tic_tac_toe/AIplayer0.h
@@ -0,0 +1,85 @@
+#pragma once
+#include "tictactoe_class.h"
+
+//approach 1 - pick the best move on that specific small board, pick random board if offered the choice
+
+int AI_0_eval(char grid[3][3], char player) {
+    tictactoe* temp = new tictactoe();
+    
+    for (int i = 0; i < 3; i++){
+        for (int j = 0; j < 3; j++){
+            temp -> grid[i][j] = grid[i][j];
+        }
+    }
+    
+    if (temp -> checkwin('O')){
+        delete temp;
+        return 1;
+    } else if (temp -> checkwin('X')){
+        delete temp;
+        return -1;
+    }
+    delete temp;
+    return 0;
+    
+}
+
+int AI_0_minimax(char board[3][3], char player) {
+    int winner = AI_0_eval(board, player);
+    
+    if(winner != 0){
+        return -1;
+    }
+
+    int move = -1;
+    int score = -2;
+    
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j<3; j++){
+            
+            if(board[i][j] == ' ') {
+                
+                board[i][j] = player;
+                
+                int thisScore = -minimax(board, (player == 'O') ? 'X' : 'O');
+                
+                if(thisScore > score) {
+                    score = thisScore;
+                    move = i;
+                }
+                board[i][j] = ' ';
+            }
+        }
+    }
+    if(move == -1){
+        return 0;
+    }
+    
+    return score;
+}
+
+
+void AI_move_0(char board[3][3], int* rowptr, int* colptr) {
+
+    int row = -1, col = -1;
+    int score = -2;
+
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++){
+            if(board[i][j] == ' ') {
+                board[i][j] = 'O';
+                
+                int tempScore = -minimax(board, 'X');
+                board[i][j] = ' ';
+                
+                if(tempScore > score) {
+                    score = tempScore;
+                    row = i;
+                    col = j;
+                }
+            }
+        }
+    }
+    *rowptr = row;
+    *colptr = col;
+}
